891c9419c618ce0d07ffacee71de8feb6a79fdae
inncent <innokentiykim90@gmail.com>
2025-09-15 22:35:20 +0500

    Hotel model added

---
+++ src/apps/hotel/hotels/domain/model.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Integer, String, ForeignKey, JSON
+
+from src.common.domain.models import Base
+
+
+class Hotels(Base):
+    __tablename__ = "hotels"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    location: Mapped[str] = mapped_column(String, nullable=False)
+    services: Mapped[dict] = mapped_column(JSON, nullable=False)
+    rooms_quantity: Mapped[int] = mapped_column(Integer, nullable=False)
+    image_id: Mapped[int] = mapped_column(Integer, nullable=False)
