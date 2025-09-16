64cff3ced7bcad823ecf60f4058320943abd8aba
inncent <innokentiykim90@gmail.com>
2025-09-17 00:03:15 +0500

    bookings model added

---
+++ src/apps/hotel/bookings/domain/model.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from decimal import Decimal
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, Integer, String, TIMESTAMP, Computed, DECIMAL
+from src.common.domain.models import Base
+import uuid
+from sqlalchemy.dialects.postgresql import ARRAY, JSONB, UUID
+
+
+class Bookings(Base):
+    __tablename__ = "bookings"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), nullable=False, primary_key=True)
+    room_id: Mapped[int] = mapped_column(ForeignKey("rooms.id", ondelete="CASCADE"), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    date_from: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), nullable=False)
+    date_to: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), nullable=False)
+    price: Mapped[Decimal] = mapped_column(DECIMAL, nullable=False)
+    total_cost: Mapped[Decimal] = mapped_column(DECIMAL, Computed("price * (date_to - date_from)"))
+    total_days: Mapped[int] = mapped_column(Integer, Computed("date_to - date_from"))
