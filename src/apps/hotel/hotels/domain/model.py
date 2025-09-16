ad72b879fe698cfa4ede68a0e6ce4c95d4b17d40
inncent <innokentiykim90@gmail.com>
2025-09-17 00:02:21 +0500

    hotels model updated

--- src/apps/hotel/hotels/domain/model.py
+++ src/apps/hotel/hotels/domain/model.py
@@ -10,6 +10,6 @@ class Hotels(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
     name: Mapped[str] = mapped_column(String, nullable=False)
     location: Mapped[str] = mapped_column(String, nullable=False)
-    services: Mapped[dict] = mapped_column(JSON, nullable=False)
+    services: Mapped[dict] = mapped_column(JSON, nullable=True)
     rooms_quantity: Mapped[int] = mapped_column(Integer, nullable=False)
-    image_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    image_id: Mapped[int] = mapped_column(Integer, nullable=True)
