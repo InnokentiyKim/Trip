83847a96eb88e99f7208b43b8f1e44ace90b105a
inncent <innokentiykim90@gmail.com>
2025-09-21 23:24:42 +0500

    common model updated: SessionDependency, ORM_OBJ, ORM_CLS types

--- src/common/domain/models.py
+++ src/common/domain/models.py
@@ -1,6 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession, AsyncAttrs, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 from src.config import create_configs
+from src.apps.hotel.bookings.domain.model import Bookings
+from src.apps.hotel.hotels.domain.model import Hotels
+from src.apps.hotel.rooms.domain.model import Rooms
 
 
 config = create_configs()
@@ -11,6 +14,9 @@ DATABASE_URL = (f"postgresql+asyncpg://{config.database.user}:{config.database.p
 engine = create_async_engine(DATABASE_URL)
 
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+ORM_OBJ = Bookings | Hotels | Rooms
+ORM_CLS = type[Bookings] | type[Hotels] | type[Rooms]
 
 
 class Base(AsyncAttrs, DeclarativeBase):
