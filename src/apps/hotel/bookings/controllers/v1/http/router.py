66dbd19bc59341946c0a7c463a0ebe6781674ff6
inncent <innokentiykim90@gmail.com>
2025-09-24 16:54:19 +0500

    booking and user routers updated

--- src/apps/hotel/bookings/controllers/v1/http/router.py
+++ src/apps/hotel/bookings/controllers/v1/http/router.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
+from apps.authentication.adapters.adapter import AuthenticationAdapter
+from apps.user.adapters.adapter import UserAdapter
+from src.apps.user.application.service import UserService
 from src.apps.hotel.bookings.adapters.adapter import BookingAdapter
 from src.apps.hotel.bookings.controllers.v1.dto.response import BookingResponseDTO
 from src.apps.hotel.bookings.application.service import BookingService
@@ -12,14 +15,20 @@ router = APIRouter(
 
 
 @router.get("")
-async def get_bookings() -> list[BookingResponseDTO]:
+async def get_bookings(request: Request) -> list[BookingResponseDTO]:
+    user_service = UserService(UserAdapter(), AuthenticationAdapter())
     service = BookingService(BookingAdapter())
-    bookings = await service.get_bookings()
+    token = request.cookies.get("token") or request.headers.get("Authorization")
+    user_id = await user_service.verify_user_by_token(token)
+    bookings = await service.get_bookings(user_id=user_id)
     return [BookingResponseDTO.from_model(booking) for booking in bookings]
 
 
 @router.get("/{booking_id}")
-async def get_bookings(booking_id: int) -> BookingResponseDTO:
+async def get_bookings(request: Request, booking_id: int) -> BookingResponseDTO:
+    user_service = UserService(UserAdapter(), AuthenticationAdapter())
     service = BookingService(BookingAdapter())
-    booking = await service.get_booking(booking_id)
+    token = request.cookies.get("token") or request.headers.get("Authorization")
+    user_id = await user_service.verify_user_by_token(token)
+    booking = await service.get_booking(booking_id, user_id=user_id)
     return BookingResponseDTO.from_model(booking)
--- src/apps/user/controllers/http/v1/router.py
+++ src/apps/user/controllers/http/v1/router.py
@@ -1,9 +1,9 @@
 from src.apps.authentication.adapters.adapter import AuthenticationAdapter
 from src.apps.user.adapters.adapter import UserAdapter
 from src.apps.user.controllers.dto.response import RegisterUserResponseDTO
-from src.apps.user.controllers.dto.request import RegisterUserRequestDTO
+from src.apps.user.controllers.dto.request import AuthUserRequestDTO
 from src.apps.user.application.service import UserService
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 
 
 router = APIRouter(
@@ -13,7 +13,15 @@ router = APIRouter(
 
 
 @router.post("/register")
-async def register_user(user_data: RegisterUserRequestDTO) -> RegisterUserResponseDTO:
+async def register_user(user_data: AuthUserRequestDTO) -> RegisterUserResponseDTO:
     service = UserService(UserAdapter(), AuthenticationAdapter())
     user = await service.register_user(user_data.email, user_data.password)
     return RegisterUserResponseDTO.from_model(user)
+
+
+@router.post("/login")
+async def login_user(response: Response, user_data: AuthUserRequestDTO) -> str:
+    service = UserService(UserAdapter(), AuthenticationAdapter())
+    access_token = await service.login_user(user_data.email, user_data.password)
+    response.set_cookie("access_token", access_token, expires=3600, httponly=True)
+    return access_token
