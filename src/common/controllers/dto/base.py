49e374fb4b33ef8e77c41a8d9d6eaed13d04a323
inncent <innokentiykim90@gmail.com>
2025-09-25 22:49:03 +0500

    common files reorganized

--- src/common/controllers/dto/base.py
+++ src/common/controllers/dto/base.py
@@ -3,3 +3,11 @@ from pydantic import BaseModel
 
 class BaseDTO(BaseModel):
     ...
+
+
+class BaseRequestDTO(BaseDTO):
+    ...
+
+
+class BaseResponseDTO(BaseDTO):
+    id: int
--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 from pydantic import Field
 
-from common.domain.enums import EnvironmentEnum
+from common.application.enums import EnvironmentEnum
 
 
 class GeneralSettings(BaseSettings):
