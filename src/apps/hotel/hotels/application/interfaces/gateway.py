51b198701f73d36dba60a51b68aac18aab889809
inncent <innokentiykim90@gmail.com>
2025-09-29 23:32:44 +0500

    gateways proto updated

---
+++ src/apps/hotel/hotels/application/interfaces/gateway.py
@@ -0,0 +1,31 @@
+from abc import abstractmethod
+
+from src.apps.hotel.hotels.domain.model import Hotel
+from src.common.interfaces import GatewayProto
+
+
+class HotelGatewayProto(GatewayProto):
+    @abstractmethod
+    async def get_hotels(self, **filters) -> list[Hotel]:
+        """Retrieve a list of hotels."""
+        ...
+
+    @abstractmethod
+    async def get_hotel_by_id(self, hotel_id: int) -> Hotel | None:
+        """Retrieve a hotel by its ID."""
+        ...
+
+    @abstractmethod
+    async def create_hotel(self, name: str, location: str, services: dict, rooms_quantity: int, image_id: int) -> None:
+        """Add a new hotel."""
+        ...
+
+    @abstractmethod
+    async def update_hotel(self, hotel_id: int, **params) -> None:
+        """Update an existing hotel."""
+        ...
+
+    @abstractmethod
+    async def delete_hotel(self, hotel_id: int) -> None:
+        """Delete a hotel by its ID."""
+        ...
--- src/apps/user/application/interfaces/gateway.py
+++ src/apps/user/application/interfaces/gateway.py
@@ -1,22 +1,22 @@
 from abc import abstractmethod
 
 from src.common.interfaces import GatewayProto
-from src.apps.user.domain.model import Users
+from src.apps.user.domain.model import User
 
 
 class UserGatewayProto(GatewayProto):
     @abstractmethod
-    async def get_user_by_id(self, user_id) -> Users:
+    async def get_user_by_id(self, user_id) -> User:
         """Retrieve a user by filters."""
         ...
 
     @abstractmethod
-    async def get_users(self, **filters) -> list[Users]:
+    async def get_users(self, **filters) -> list[User]:
         """Retrieve a list of users."""
         ...
 
     @abstractmethod
-    async def add_user(self, user: Users) -> None:
+    async def add_user(self, user: User) -> None:
         """Add a new user."""
         ...
 
