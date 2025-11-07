c098c1cc5c2bcdd5271c9fdd0ea553b04d34df5c
inncent <innokentiykim90@gmail.com>
2025-11-07 22:58:27 +0500

    feat: ioc registry

---
+++ src/ioc_registry.py
@@ -0,0 +1,18 @@
+from dishka import Provider
+
+from apps.hotel.ioc import get_hotel_providers
+
+
+def get_providers() -> list[Provider]:
+    """
+    Get the list of all providers.
+
+    This function aggregates and returns a list of all provider instances,
+    including infrastructure providers.
+
+    Returns:
+        list[Provider]: A list of provider instances.
+    """
+    return [
+        *get_hotel_providers()
+    ]
