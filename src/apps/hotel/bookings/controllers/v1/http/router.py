d5c1676a96bd5fab2d45a8860345cfbdf694140a
inncent <innokentiykim90@gmail.com>
2025-11-10 00:50:51 +0500

    refactor: booking router with injections

--- src/apps/hotel/bookings/controllers/v1/http/router.py
+++ src/apps/hotel/bookings/controllers/v1/http/router.py
@@ -1,11 +1,12 @@
 from uuid import UUID
 
-from fastapi import APIRouter, Request
+from fastapi import APIRouter
+from dishka.integrations.fastapi import FromDishka, inject
+from src.common.utils.auth_scheme import auth_header
 
-from src.infrastructure.security.adapters.adapter import SecurityAdapter
-from src.apps.user.adapters.adapter import UserAdapter
+from apps.user.application.exceptions import UserNotFoundException
+from common.exceptions.handlers import generate_responses
 from src.apps.user.application.service import UserService
-from src.apps.hotel.bookings.adapters.adapter import BookingAdapter
 from src.apps.hotel.bookings.controllers.v1.dto.response import BookingResponseDTO
 from src.apps.hotel.bookings.application.service import BookingService
 from src.common.controllers.dto.base import BaseResponseDTO
@@ -17,41 +18,72 @@ router = APIRouter(
 )
 
 
-@router.get("")
-async def get_bookings(request: Request) -> list[BookingResponseDTO]:
-    user_service = UserService(UserAdapter(), SecurityAdapter())
-    service = BookingService(BookingAdapter())
-    token = request.cookies.get("token") or request.headers.get("Authorization")
-    user = await user_service.verify_user_by_token(token)
-    bookings = await service.get_bookings(user.id)
+@router.get(
+    "",
+    responses=generate_responses(
+        UserNotFoundException,
+    )
+)
+@inject
+async def get_bookings(
+    user_service: FromDishka[UserService],
+    booking_service: FromDishka[BookingService],
+    token: str = auth_header
+) -> list[BookingResponseDTO]:
+    user = await user_service.verify_user_by_token(token=token)
+    bookings = await booking_service.get_bookings(user.id)
     return [BookingResponseDTO.from_model(booking) for booking in bookings]
 
 
-@router.get("/{booking_id}")
-async def get_bookings(request: Request, booking_id: UUID) -> BookingResponseDTO:
-    user_service = UserService(UserAdapter(), SecurityAdapter())
-    service = BookingService(BookingAdapter())
-    token = request.cookies.get("token") or request.headers.get("Authorization")
+@router.get(
+    "/{booking_id}",
+    responses=generate_responses(
+        UserNotFoundException,
+    )
+)
+@inject
+async def get_bookings(
+    booking_id: UUID,
+    user_service: FromDishka[UserService],
+    booking_service: FromDishka[BookingService],
+    token: str = auth_header
+) -> BookingResponseDTO:
     user = await user_service.verify_user_by_token(token)
-    booking = await service.get_booking(user.id, booking_id)
+    booking = await booking_service.get_booking(user.id, booking_id)
     return BookingResponseDTO.from_model(booking)
 
 
-@router.post("/{booking_id}")
-async def cancel_active_booking(request: Request, booking_id: UUID) -> BaseResponseDTO:
-    user_service = UserService(UserAdapter(), SecurityAdapter())
-    service = BookingService(BookingAdapter())
-    token = request.cookies.get("token") or request.headers.get("Authorization")
+@router.post(
+    "/{booking_id}",
+    responses=generate_responses(
+        UserNotFoundException,
+    )
+)
+@inject
+async def cancel_active_booking(
+    booking_id: UUID,
+    user_service: FromDishka[UserService],
+    booking_service: FromDishka[BookingService],
+    token: str = auth_header
+) -> BaseResponseDTO:
     user = await user_service.verify_user_by_token(token)
-    await service.cancel_active_booking(user.id, booking_id)
+    await booking_service.cancel_active_booking(user.id, booking_id)
     return BaseResponseDTO(id=booking_id)
 
 
-@router.delete("/{booking_id}")
-async def delete_booking(request: Request, booking_id: UUID) -> BaseResponseDTO:
-    user_service = UserService(UserAdapter(), SecurityAdapter())
-    service = BookingService(BookingAdapter())
-    token = request.cookies.get("token") or request.headers.get("Authorization")
+@router.delete(
+    "/{booking_id}",
+    responses=generate_responses(
+        UserNotFoundException,
+    )
+)
+@inject
+async def delete_booking(
+    booking_id: UUID,
+    user_service: FromDishka[UserService],
+    booking_service: FromDishka[BookingService],
+    token: str = auth_header
+) -> BaseResponseDTO:
     user = await user_service.verify_user_by_token(token)
-    await service.delete_booking(user.id, booking_id)
+    await booking_service.delete_booking(user.id, booking_id)
     return BaseResponseDTO(id=booking_id)
