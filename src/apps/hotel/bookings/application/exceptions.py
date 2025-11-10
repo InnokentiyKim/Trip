4cc644341f18ccc8006f2aa4ea7fc0a7002b9b51
inncent <innokentiykim90@gmail.com>
2025-11-11 00:40:13 +0500

    refactor: booking exceptions updated

--- src/apps/hotel/bookings/application/exceptions.py
+++ src/apps/hotel/bookings/application/exceptions.py
@@ -1,23 +1,24 @@
 from common.exceptions.common import BaseError
+from fastapi import status
 
 
 class BookingNotFoundException(BaseError):
     """Exception raised when a booking is not found."""
-    status_code = 404
+    status_code = status.HTTP_404_NOT_FOUND
     message = "Booking not found."
 
 
 class BookingAlreadyExistsException(BaseError):
     """Exception raised when trying to create a booking that already exists."""
-    status_code = 409
+    status_code = status.HTTP_409_CONFLICT
     message = "Booking already exists."
 
 
-class BookingAlreadyDeletedException(BaseError):
-    status_code = 409
-    message = "Booking already deleted."
-
-
 class BookingProcessingErrorException(BaseError):
-    status_code = 500
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     message = "Booking processing error."
+
+
+class RoomCannotBeBookedException(BaseError):
+    status_code = status.HTTP_400_BAD_REQUEST
+    message = "Room cannot be booked."
