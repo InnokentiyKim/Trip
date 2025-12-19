4012a50d3501f2913799b80144553b38318b4cf6
inncent <innokentiykim90@gmail.com>
2025-12-19 16:05:41 +0400

    feat: logging integration for room service

--- src/apps/hotel/rooms/application/ensure.py
+++ src/apps/hotel/rooms/application/ensure.py
@@ -2,16 +2,19 @@ from src.apps.hotel.rooms.application.interfaces.gateway import RoomGatewayProto
 from src.apps.hotel.rooms.domain.models import Room
 from src.common.application.ensure import ServiceInsuranceBase
 from src.apps.hotel.rooms.application import exceptions
+from src.common.interfaces import CustomLoggerProto
 
 
 class RoomServiceInsurance(ServiceInsuranceBase):
     """Room service ensuring."""
 
-    def __init__(self, gateway: RoomGatewayProto) -> None:
+    def __init__(self, gateway: RoomGatewayProto, logger: CustomLoggerProto) -> None:
         self._room = gateway
+        self._logger = logger
 
     async def room_exists(self, hotel_id: int, room_id: int) -> Room:
         room = await self._room.get_room(hotel_id, room_id)
         if room is None:
+            self._logger.error("Room not found", hotel_id=hotel_id, room_id=room_id)
             raise exceptions.RoomNotFoundException
         return room
--- src/apps/hotel/rooms/application/service.py
+++ src/apps/hotel/rooms/application/service.py
@@ -4,6 +4,7 @@ from src.apps.hotel.rooms.application import exceptions
 from src.apps.hotel.rooms.application.interfaces.gateway import RoomGatewayProto
 from src.apps.hotel.rooms.domain.models import Room
 from src.common.application.service import ServiceBase
+from src.common.interfaces import CustomLoggerProto
 
 
 class RoomService(ServiceBase):
@@ -11,10 +12,13 @@ class RoomService(ServiceBase):
         self,
         # hotel_gateway: HotelGatewayProto,
         room_gateway: RoomGatewayProto,
+        logger: CustomLoggerProto,
     ) -> None:
         self._room_adapter = room_gateway
+        self._logger = logger
         # self._hotel_ensure = HotelServiceInsurance(hotel_gateway)
-        self._room_ensure = RoomServiceInsurance(room_gateway)
+        self._room_ensure = RoomServiceInsurance(room_gateway, logger)
+        super().__init__()
 
     async def list_rooms(self, cmd: commands.ListRoomsCommand) -> list[Room]:
         params = cmd.model_dump(exclude={"hotel_id"}, exclude_unset=True)
@@ -24,6 +28,7 @@ class RoomService(ServiceBase):
     async def get_room(self, cmd: commands.GetRoomCommand) -> Room:
         room = await self._room_adapter.get_room(cmd.hotel_id, cmd.room_id)
         if room is None:
+            self._logger.error("Room not found", hotel_id=cmd.hotel_id, room_id=cmd.room_id)
             raise exceptions.RoomNotFoundException
         return room
 
@@ -39,7 +44,10 @@ class RoomService(ServiceBase):
             cmd.image_id,
         )
         if room_id is None:
+            self._logger.error("Room already exists", hotel_id=cmd.hotel_id, name=cmd.name)
             raise exceptions.RoomAlreadyExistsException
+
+        self._logger.info("New room successfully added", hotel_id=cmd.hotel_id, room_id=room_id)
         return room_id
 
     async def update_room(self, cmd: commands.UpdateRoomCommand) -> int:
@@ -51,10 +59,15 @@ class RoomService(ServiceBase):
         )
         updated_room_id = await self._room_adapter.update_room(room, **updating_params)
         if updated_room_id is None:
+            self._logger.error("Room cannot be updated", hotel_id=cmd.hotel_id, room_id=cmd.room_id)
             raise exceptions.RoomCannotBeUpdatedException
+
+        self._logger.info("Room successfully updated", hotel_id=cmd.hotel_id, room_id=cmd.room_id)
         return updated_room_id
 
     async def delete_room(self, cmd: commands.DeleteRoomCommand) -> None:
         # hotel = await self._hotel_ensure.users_hotel_exists(cmd.user_id, cmd.hotel_id)
         room = await self._room_ensure.room_exists(cmd.hotel_id, cmd.room_id)
+
         await self._room_adapter.delete_room(room)
+        self._logger.info("Room successfully deleted", hotel_id=cmd.hotel_id, room_id=cmd.room_id)
