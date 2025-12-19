eb549b1372b35ba499bf533621b9e72d7be9c8eb
inncent <innokentiykim90@gmail.com>
2025-12-19 16:05:24 +0400

    feat: logging integration for hotel service

--- src/apps/hotel/hotels/application/ensure.py
+++ src/apps/hotel/hotels/application/ensure.py
@@ -4,22 +4,26 @@ from src.apps.hotel.hotels.domain.models import Hotel
 from src.apps.hotel.hotels.application.interfaces.gateway import HotelGatewayProto
 from src.common.application.ensure import ServiceInsuranceBase
 from src.apps.hotel.hotels.application import exceptions
+from src.common.interfaces import CustomLoggerProto
 
 
 class HotelServiceInsurance(ServiceInsuranceBase):
     """Hotel service ensuring."""
 
-    def __init__(self, gateway: HotelGatewayProto) -> None:
+    def __init__(self, gateway: HotelGatewayProto, logger: CustomLoggerProto) -> None:
         self._hotel = gateway
+        self._logger = logger
 
     async def hotel_exists(self, hotel_id: int) -> Hotel:
         hotel = await self._hotel.get_hotel_by_id(hotel_id)
         if hotel is None:
+            self._logger.error("Hotel not found", hotel_id=hotel_id)
             raise exceptions.HotelNotFoundException
         return hotel
 
     async def users_hotel_exists(self, user_id: UUID, hotel_id: int) -> Hotel:
         hotel = await self._hotel.get_users_hotel(user_id, hotel_id)
         if hotel is None:
+            self._logger.error("User's hotel not found", user_id=user_id, hotel_id=hotel_id)
             raise exceptions.HotelNotFoundException
         return hotel
--- src/apps/hotel/hotels/application/service.py
+++ src/apps/hotel/hotels/application/service.py
@@ -4,12 +4,14 @@ from src.apps.hotel.hotels.application.interfaces.gateway import HotelGatewayPro
 from src.apps.hotel.hotels.domain.models import Hotel
 from src.common.application.service import ServiceBase
 from src.apps.hotel.hotels.application.ensure import HotelServiceInsurance
+from src.common.interfaces import CustomLoggerProto
 
 
 class HotelService(ServiceBase):
-    def __init__(self, gateway: HotelGatewayProto) -> None:
+    def __init__(self, gateway: HotelGatewayProto, logger: CustomLoggerProto) -> None:
         self._adapter = gateway
-        self._ensure = HotelServiceInsurance(gateway)
+        self._logger = logger
+        self._ensure = HotelServiceInsurance(gateway, logger)
 
     async def list_hotels(self, cmd: commands.ListHotelsCommand) -> list[Hotel]:
         params = cmd.model_dump(exclude_unset=True)
@@ -21,19 +23,24 @@ class HotelService(ServiceBase):
         return hotel
 
     async def create_hotel(self, cmd: commands.CreateHotelCommand) -> int:
-        params = cmd.model_dump(exclude_unset=True)
 
         hotel = Hotel(
-            name=params.pop("name"),
-            location=params.pop("location"),
-            rooms_quantity=params.pop("rooms_quantity"),
-            owner=params.pop("owner"),
+            name=cmd.name,
+            location=cmd.location,
+            rooms_quantity=cmd.rooms_quantity,
+            owner=cmd.owner,
+            services=cmd.services,
+            image_id=cmd.image_id,
         )
-        for key, value in params.items():
-            setattr(hotel, key, value)
+        if cmd.is_active:
+            hotel.is_active = cmd.is_active
+
         new_hotel_id = await self._adapter.add_hotel(hotel)
         if new_hotel_id is None:
+            self._logger.error("Hotel creation failed, hotel already exists", name=cmd.name, location=cmd.location)
             raise exceptions.HotelAlreadyExistsException
+
+        self._logger.info(f"New hotel successfully created", hotel_id=new_hotel_id, owner=str(cmd.owner))
         return new_hotel_id
 
     async def update_hotel(self, cmd: commands.UpdateHotelCommand) -> int:
@@ -41,9 +48,14 @@ class HotelService(ServiceBase):
         params = cmd.model_dump(exclude={"hotel_id"}, exclude_unset=True)
         is_updated = await self._adapter.update_hotel(hotel, **params)
         if is_updated is None:
+            self._logger.error("Hotel update failed", hotel_id=cmd.hotel_id)
             raise exceptions.HotelCannotBeUpdatedException
+
+        self._logger.info("Hotel's info successfully updated", hotel_id=cmd.hotel_id)
         return is_updated
 
     async def delete_hotel(self, cmd: commands.DeleteHotelCommand) -> None:
         hotel = await self._ensure.hotel_exists(cmd.hotel_id)
+
         await self._adapter.delete_hotel(hotel)
+        self._logger.info(f"Hotel successfully deleted", hotel_id=cmd.hotel_id)
