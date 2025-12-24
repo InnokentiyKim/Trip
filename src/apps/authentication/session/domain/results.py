c9422441b30d2260f4d7371b3baff9618f0a7438
inncent <innokentiykim90@gmail.com>
2025-12-24 21:00:08 +0400

    refactor: auth and user results

--- src/apps/authentication/session/domain/results.py
+++ src/apps/authentication/session/domain/results.py
@@ -4,7 +4,9 @@ from uuid import UUID
 
 from pydantic import SecretStr
 
-from src.apps.authentication.user.domain.models import User, AuthStatus
+from src.apps.authentication.session.domain.enums import PasswordResetTokenStatusEnum, OTPStatusEnum
+from src.apps.authentication.user.domain.models import AuthStatus
+from src.apps.notification.enums import NotificationChannelEnum
 
 
 @dataclass(slots=True, frozen=True)
@@ -16,9 +18,43 @@ class OAuthProviderUser:
 
 
 @dataclass(slots=True, frozen=True)
+class UserID:
+    id: UUID
+
+
+@dataclass(slots=True, frozen=True)
 class AuthTokens:
     access_token: SecretStr
     refresh_token: SecretStr
+
+
+@dataclass(slots=True, frozen=True)
+class PasswordResetTokenInfo:
+    id: UUID
+    user_id: UUID
+    reset_token: SecretStr
+    hashed_reset_token: str
+    created_at: datetime
+    expires_at: datetime
+    status: PasswordResetTokenStatusEnum
+
+
+@dataclass(slots=True, frozen=True)
+class OTPCodeInfo:
+    id: UUID
+    user_id: UUID
+    channel: NotificationChannelEnum
+    otp_code: str
+    hashed_otp_code: str
+    failed_attempts: int
+    created_at: datetime
+    expires_at: datetime
+    status: OTPStatusEnum
+
+
+@dataclass(slots=True, frozen=True)
+class MFAToken:
+    mfa_token: SecretStr
 
 
 @dataclass(slots=True, frozen=True)
@@ -46,39 +82,3 @@ class AuthInfo:
     def is_mfa_enabled(self) -> bool:
         """Returns True if any MFA method is enabled."""
         return self.mfa_email_enabled or self.mfa_sms_enabled
-
-
-@dataclass(slots=True, frozen=True)
-class UserInfo:
-    id: UUID
-    email: str
-    phone: str | None
-    name: str | None
-    avatar_url: str | None
-    created_at: datetime
-    updated_at: datetime
-
-    auth_info: AuthInfo
-
-    @classmethod
-    def from_model(cls, model: User) -> "UserInfo":
-        """
-        Converts `User` domain entity to the `UserInfo` structure.
-
-        Args:
-            model: The `User` entity to convert from.
-
-        Returns:
-            UserInfo: A `UserInfo` structure representing the user data.
-        """
-        auth_info = AuthInfo.from_model(model.auth_status)
-        return cls(
-            id=model.id,
-            email=model.email,
-            phone=model.phone,
-            name=model.name,
-            avatar_url=model.avatar_url,
-            created_at=model.created_at,
-            updated_at=model.updated_at,
-            auth_info=auth_info,
-        )
