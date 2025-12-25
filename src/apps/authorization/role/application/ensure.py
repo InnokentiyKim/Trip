61187438e26a7d76a3b605482cf2152bc9ac4d77
inncent <innokentiykim90@gmail.com>
2025-12-25 22:53:38 +0400

    feat: RoleManagementService with Ensurance

---
+++ src/apps/authorization/role/application/ensure.py
@@ -0,0 +1,92 @@
+from uuid import UUID
+
+from src.apps.authorization.access.domain.models import Role, Permission
+from src.apps.authorization.role.application.interfaces.gateway import RoleGatewayProto, PermissionGatewayProto
+from src.apps.authorization.role.domain.enums import BaseRoleEnum, UserRoleEnum
+from src.common.interfaces import CustomLoggerProto
+from src.apps.authorization.role.application import exceptions
+
+
+class RoleInteractorEnsurance:
+    def __init__(self, roles: RoleGatewayProto, logger: CustomLoggerProto) -> None:
+        self._roles = roles
+        self._logger = logger
+
+    async def role_exists(self, role_id: UUID) -> Role:
+        """
+        Ensure that a role with the given ID exists.
+
+        Args:
+            role_id (UUID): The ID of the role to check.
+
+        Returns:
+            Role: The role object.
+
+        Raises:
+            RoleIsNotFoundError: If the role does not exist.
+        """
+        role = await self._roles.get(role_id)
+
+        if not role:
+            self._logger.error("Role is not found", role_id=role_id)
+            raise exceptions.RoleIsNotFoundError from None
+
+        return role
+
+    async def role_name_is_not_base(self, role_name: UserRoleEnum) -> None:
+        """
+        Ensure that the role code is not a base role.
+
+        Args:
+            role_name (UserRoleEnum): The name of the role to check.
+
+        Raises:
+            RoleCouldNotBeDeletedError: If the role code is a base role.
+        """
+        # Prohibit to delete base roles
+        if role_name in BaseRoleEnum:
+            self._logger.error("Base role could not be removed")
+            raise exceptions.RoleCouldNotBeDeletedError from None
+
+    async def no_users_granted_to_role(self, role_id: UUID) -> None:
+        """
+        Ensure that no users are granted to the specified role.
+
+        Args:
+            role_id (UUID): The role's ID to check.
+
+        Raises:
+            RoleCouldNotBeDeletedError: If there are users granted to the role.
+        """
+        if await self._roles.get_all_users_granted_to_role(role_id):
+            self._logger.error("Role could not be deleted because there are users granted to it", role_id=role_id)
+            raise exceptions.RoleCouldNotBeDeletedError from None
+
+    @staticmethod
+    async def ensure_permissions_exist(
+        permissions_ids: list[UUID], permissions: PermissionGatewayProto, logger: CustomLoggerProto
+    ) -> list[Permission]:
+        """
+        Ensures that all specified permission IDs exist in the system.
+
+        Args:
+            permissions_ids (list[UUID]): List of permission UUIDs to check.
+            permissions (PermissionsGatewayProto): Gateway to access permissions.
+            logger (CustomLoggerProto): Logger for error reporting.
+
+        Returns:
+            list[Permission]: List of found Permission objects.
+
+        Raises:
+            PermissionsNotFoundError: If any of the specified permissions do not exist.
+        """
+        permission_objects = await permissions.get_list(permissions_ids)
+        found_permissions = [permission.id for permission in permission_objects]
+
+        if len(found_permissions) < len(permissions_ids):
+            not_found_permissions = set(permissions_ids) - set(found_permissions)
+            error_message = f"Some of permissions do not exist {not_found_permissions}"
+            logger.error(error_message)
+            raise exceptions.PermissionsNotFoundError(message=error_message)
+
+        return permission_objects
---
+++ src/apps/authorization/role/application/service.py
@@ -0,0 +1,141 @@
+import uuid
+
+from src.apps.authorization.access.domain.models import Role
+from src.apps.authorization.role.application.ensure import RoleInteractorEnsurance
+from src.apps.authorization.role.application.interfaces.gateway import RoleGatewayProto, PermissionGatewayProto
+from src.common.interfaces import CustomLoggerProto
+from src.apps.authorization.role.domain import commands
+from src.apps.authorization.role.domain import results
+from src.apps.authorization.role.domain import fetches
+
+
+class RoleManagementService:  # noqa: WPS214
+    def __init__(
+        self,
+        gateway: RoleGatewayProto,
+        permissions: PermissionGatewayProto,
+        logger: CustomLoggerProto,
+    ) -> None:
+        self._gateway = gateway
+        self._permissions = permissions
+        self._logger = logger
+        self._ensure = RoleInteractorEnsurance(gateway, logger)
+
+    async def create_role(
+        self, cmd: commands.CreateCustomRole
+    ) -> results.RoleInfo:
+        """
+        Creates a new custom role with the specified permissions.
+
+        Args:
+            cmd (CreateCustomRole): Command containing role details and permissions.
+
+        Returns:
+            RoleFullInfo: Information about the created role.
+
+        Raises:
+            PermissionsNotFoundError: If any of the specified permissions do not exist.
+            RoleAlreadyExistsError: If a role with the same code already exists.
+        """
+        permissions = await self._permissions.get_list(cmd.permissions)
+
+        # Create role
+        role = Role(
+            id=uuid.uuid4(),
+            name=cmd.name,
+            description=cmd.description,
+        )
+        await role.add_permissions(permissions)
+        await self._gateway.add(role)
+
+        self._logger.debug("Role created", role_id=role.id, role_name=cmd.name)
+        return results.RoleInfo.from_model(role)
+
+    async def delete_role(
+        self, cmd: commands.DeleteRole
+    ) -> results.RoleInfo:
+        """
+        Deletes a role by its ID.
+
+        Args:
+            cmd (src.apps.authorization.role.domain.commands.DeleteRole): Command containing the role ID to delete.
+
+        Raises:
+            RoleIsNotFoundError: If the role does not exist.
+            RoleCouldNotBeDeletedError: If the role is a base role and cannot be deleted.
+            RoleAssignedToUsersError: If the role is assigned to users and cannot be deleted.
+        """
+        # Ensure role exists and can be deleted
+        role = await self._ensure.role_exists(cmd.role_id)
+        await self._ensure.role_name_is_not_base(role.name)
+        await self._ensure.no_users_granted_to_role(role.id)
+
+        await self._gateway.delete(role.id)
+
+        self._logger.debug("Role deleted", role_id=role.id, role_name=role.name)
+        return results.RoleInfo.from_model(role)
+
+    async def get_role_info(self, fetch: fetches.GetRoleInfo) -> results.RoleInfo:
+        """
+        Retrieves detailed information about a role, including its permissions.
+
+        Args:
+            fetch (GetRoleInfo): Object containing the role ID to fetch.
+
+        Returns:
+            RoleFullInfo: Information about the role and its permissions.
+
+        Raises:
+            RoleIsNotFoundError: If the role does not exist.
+        """
+        role = await self._ensure.role_exists(fetch.role_id)
+
+        self._logger.debug("Role info retrieved", role_id=role.id, role_name=role.name)
+        return results.RoleInfo.from_model(role)
+
+    async def assign_permissions_to_role(
+        self, cmd: commands.AssignPermissionsToRole
+    ) -> results.RoleInfo:
+        """
+        Assigns a list of permissions to a specific role.
+
+        Args:
+            cmd (AssignPermissionsToRole): Command containing the role ID and permissions to assign.
+
+        Returns:
+            RoleFullInfo: Information about the role with its updated permissions.
+
+        Raises:
+            RoleIsNotFoundError: If the role does not exist.
+            PermissionsNotFoundError: If any of the specified permissions do not exist.
+        """
+        role = await self._ensure.role_exists(cmd.role_id)
+        permissions = await self._permissions.get_list(cmd.permissions)
+
+        await role.add_permissions(permissions)
+        await self._gateway.add(role)
+
+        self._logger.debug("Permissions assigned to role", role_id=role.id, role_name=role.name)
+        return results.RoleInfo.from_model(role)
+
+    async def remove_permissions_from_role(
+        self, cmd: commands.RemovePermissionsFromRole
+    ) -> results.RoleInfo:
+        """
+        Removes specified permissions from a role.
+
+        Args:
+            cmd (RemovePermissionsFromRole): Command containing the role ID and permissions to remove.
+
+        Raises:
+            RoleIsNotFoundError: If the role does not exist.
+        """
+        async with self._gateway():
+            permissions = await self._permissions.get_list(cmd.permissions)
+
+            role = await self._ensure.role_exists(cmd.role_id)
+            await role.remove_permissions(permissions)
+            await self._gateway.add(role)
+
+            self._logger.debug("Permissions removed from role", role_id=role.id, role_name=role.name)
+            return results.RoleInfo.from_model(role)
