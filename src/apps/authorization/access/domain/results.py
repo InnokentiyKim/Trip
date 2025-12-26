76b64a5b9545fc0762a08475438c05104ee4a45f
inncent <innokentiykim90@gmail.com>
2025-12-27 00:03:53 +0400

    feat: Access results

---
+++ src/apps/authorization/access/domain/results.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from uuid import UUID
+
+from src.apps.authorization.access.domain.enums import ResourceTypeEnum, PermissionEnum
+from src.apps.authorization.role.domain.enums import UserRoleEnum
+
+
+@dataclass(slots=True, frozen=True)
+class UserAccessInfo:
+    user_id: UUID
+    role: PermissionEnum | UserRoleEnum
+    resource_id: UUID
+    resource_type: ResourceTypeEnum
+
+    @property
+    def user_type(self) -> ResourceTypeEnum:
+        """Get the user resource type."""
+        return ResourceTypeEnum.USER
