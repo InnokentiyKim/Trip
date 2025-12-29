0bad94ae456a8d115692035624ce3819b2179d65
inncent <innokentiykim90@gmail.com>
2025-12-30 00:12:10 +0400

    feat: authentication ioc

--- src/apps/authentication/ioc.py
+++ src/apps/authentication/ioc.py
@@ -1,9 +1,12 @@
 from dishka import Provider, Scope, provide, provide_all
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.apps.authentication.session.adapters.adapter import AuthSessionAdapter
+from src.apps.authentication.session.adapters.adapter import AuthSessionAdapter, PasswordResetTokenAdapter, \
+    OTPCodeAdapter
 from src.apps.authentication.session.application.ensure import AuthenticationServiceEnsurance
-from src.apps.authentication.session.application.interfaces.gateway import AuthSessionGatewayProto
+from src.apps.authentication.session.application.interfaces.gateway import (
+    AuthSessionGatewayProto, PasswordResetTokenGatewayProto, OTPCodeGatewayProto
+)
 from src.apps.authentication.session.application.service import AuthenticationService
 from src.apps.authentication.user.adapters.adapter import UserAdapter
 from src.apps.authentication.user.application.ensure import UserServiceEnsurance
@@ -33,8 +36,16 @@ class AuthenticationGatewayProviders(Provider):
     def provide_auth_session_adapter(self, session: AsyncSession) -> AuthSessionGatewayProto:
         return AuthSessionAdapter(session)
 
+    @provide(provides=PasswordResetTokenGatewayProto)
+    def provide_password_reset_token_adapter(self, session: AsyncSession) -> PasswordResetTokenGatewayProto:
+        return PasswordResetTokenAdapter(session)
 
-def get_user_providers() -> list[Provider]:
+    @provide(provides=OTPCodeGatewayProto)
+    def provide_otp_code_adapter(self, session: AsyncSession) -> OTPCodeGatewayProto:
+        return OTPCodeAdapter(session)
+
+
+def get_authentication_providers() -> list[Provider]:
     return [
         AuthenticationServiceProviders(),
         AuthenticationGatewayProviders()
