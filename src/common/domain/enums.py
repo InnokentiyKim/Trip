765cdf68f5555141f66269234e067d5a1e7c4f5c
inncent <innokentiykim90@gmail.com>
2026-01-04 23:39:12 +0400

    tests: enums updated for testing

--- src/common/domain/enums.py
+++ src/common/domain/enums.py
@@ -6,3 +6,28 @@ class EnvironmentEnum(StrEnum):
     DEV = "dev"
     STAGE = "stage"
     PROD = "prod"
+
+
+class DataAccessEnum(StrEnum):
+    ALCHEMY = "alchemy"
+    MEMORY = "memory"
+    HTTP = "http"
+    RABBITMQ = "rabbitmq"
+    S3 = "s3"
+
+
+class EmailAdapterEnum(StrEnum):
+    SMTP = "smtp"
+    NAVER = "naver"
+    MEMORY = "memory"
+
+
+class SMSAdapterEnum(StrEnum):
+    NAVER = "naver"
+    MEMORY = "memory"
+
+
+class RunModeEnum(StrEnum):
+    HTTP = "http"
+    AMQP = "amqp"
+    FULL = "full"
