dbb60b51f761c3e4b05b931a05e90ad2fbdd4b22
inncent <innokentiykim90@gmail.com>
2026-01-05 21:38:46 +0400

    tests: fake comment adapter implementation

--- src/apps/comment/adapters/adapter.py
+++ src/apps/comment/adapters/adapter.py
@@ -3,7 +3,7 @@ from uuid import UUID
 from src.apps.comment.application.interfaces.gateway import CommentGatewayProto
 from src.apps.comment.domain.excepitions import CommentAlreadyExistsException
 from src.apps.comment.domain.models import Comment
-from src.common.adapters.adapter import SQLAlchemyGateway
+from src.common.adapters.adapter import SQLAlchemyGateway, FakeGateway
 from sqlalchemy import select
 
 
@@ -47,3 +47,35 @@ class CommentAdapter(SQLAlchemyGateway, CommentGatewayProto):
     async def delete_comment(self, comment: Comment) -> None:
         """Delete a comment by its ID."""
         await self.delete_item(comment)
+
+
+class FakeCommentAdapter(FakeGateway, CommentGatewayProto):
+    async def add(self, comment: Comment) -> None:
+        """Add a new comment."""
+        self._collection.add(comment)
+
+    async def get_comment_by_id(self, comment_id: UUID) -> Comment:
+        """Retrieve a comment by its ID."""
+        return next((comment for comment in self._collection if comment.id == comment_id), None)
+
+    async def get_comments_by_user_id(self, user_id: UUID) -> list[Comment]:
+        """Retrieve a list of comments made by a specific user."""
+        return [comment for comment in self._collection if comment.user_id == user_id]
+
+    async def get_comments_by_hotel_id(self, hotel_id: int) -> list[Comment]:
+        """Retrieve a list of comments for a specific hotel."""
+        return [comment for comment in self._collection if comment.hotel_id == hotel_id]
+
+    async def update_comment(self, comment: Comment, **params) -> UUID | None:
+        """Update an existing comment."""
+        for key, value in params.items():
+            setattr(comment, key, value)
+
+        self._collection.discard(comment)
+        self._collection.add(comment)
+
+        return comment.id or None
+
+    async def delete_comment(self, comment: Comment) -> None:
+        """Delete a comment by its ID."""
+        self._collection.discard(comment)
