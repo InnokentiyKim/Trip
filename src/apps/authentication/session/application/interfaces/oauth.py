87ac4333696f335de2a8fbd40041f62172a99e07
inncent <innokentiykim90@gmail.com>
2026-01-26 23:21:33 +0400

    feat: Base interface for OAuth

---
+++ src/apps/authentication/session/application/interfaces/oauth.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+
+from src.apps.authentication.session.domain.results import OAuthProviderUser, OAuthConnectorData
+
+
+class OAuthAdapterProto(ABC):
+    @abstractmethod
+    async def authorize(self, auth_code: str) -> None:
+        """
+        Exchange an authorization code for an access token and authorize the adapter.
+
+        Args:
+            auth_code (str): The authorization code received from the OAuth
+            provider's redirect URL query parameters.
+        """
+        ...
+
+    @abstractmethod
+    async def get_user_info(self) -> OAuthProviderUser:
+        """
+        Fetch the authenticated user's profile information from the provider.
+
+        Returns:
+            OAuthProviderUser: A data structure containing the user's profile
+            information obtained from the provider.
+        """
+        ...
+
+    @abstractmethod
+    async def get_token_data(self) -> OAuthConnectorData:
+        """
+        Extract the token data from the provider.
+
+        Returns:
+            OAuthConnectorData: A data structure containing the token data
+        """
+        ...
