5d78f8dd967b6651e0c9f89468ba72d07e514874
inncent <innokentiykim90@gmail.com>
2026-02-02 19:38:39 +0400

    feat: sqlalchemy UoW implementation

--- src/infrastructure/database/factory.py
+++ src/infrastructure/database/factory.py
@@ -1,5 +1,10 @@
-from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+from types import TracebackType
+from typing import Self
 
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, AsyncSessionTransaction, create_async_engine
+
+from src.common.interfaces import UowProto
+from src.infrastructure.context import RequestContext
 from src.infrastructure.database.postgres.config import DatabaseSettings
 
 
@@ -10,3 +15,49 @@ def create_database_adapter(config: DatabaseSettings) -> AsyncEngine:
         config (DatabaseSettings): The database configuration settings.
     """
     return create_async_engine(config.db_url, **config.engine.model_dump())
+
+
+class SqlAlchemyUnitOfWork(UowProto):
+    """SQLAlchemy unit of work implementation.
+
+    The class wraps the already existing SQLAlchemy unit of work implementations for easier usage.
+    """
+
+    def __init__(
+        self,
+        session: AsyncSession,
+        request_context: RequestContext,
+        transaction: AsyncSessionTransaction | None = None,
+    ) -> None:
+        self.session: AsyncSession = session
+        self.request_context = request_context
+        self.transaction: AsyncSessionTransaction | None = transaction
+
+    async def commit(self) -> None:
+        """Commit the current transaction."""
+        if self.transaction is not None:
+            await self.transaction.commit()
+
+    async def rollback(self) -> None:
+        """Roll back the current transaction."""
+        if self.transaction is not None:
+            await self.transaction.rollback()
+
+    async def __aenter__(self) -> Self:
+        """Start a transaction on the current session and return the UoW instance."""
+        self.transaction = self.transaction or self.session.begin()
+        await self.transaction.__aenter__()
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_value: BaseException | None,
+        traceback: TracebackType | None,
+    ) -> None:
+        """Stop the current transaction and commit or rollback based on the result."""
+        if self.transaction is not None:
+            await self.transaction.__aexit__(exc_type, exc_value, traceback)
+        self.transaction = None
+
+        return
