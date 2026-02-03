41d6c1c0617688646842fa32b6e26bff7078fb17
inncent <innokentiykim90@gmail.com>
2026-02-03 23:51:46 +0400

    feat: oauth exceptions

---
+++ src/apps/authentication/session/adapters/oauth/exceptions.py
@@ -0,0 +1,27 @@
+from src.common.exceptions.common import BaseError
+from fastapi import status
+
+
+class UnsupportedOAuthProviderError(BaseError):
+    """
+    Exception raised for unsupported OAuth adapters.
+
+    This exception is raised when an OAuth adapter is used that
+    is not supported by the system.
+    """
+    status_code = status.HTTP_401_UNAUTHORIZED
+    message = "Unsupported OAuth provider."
+    loc = "oauth"
+
+
+class OAuthProviderLoginError(BaseError):
+    """
+    Exception raised when there is an issue during the login process with an OAuth provider.
+
+    This exception can be used to signal errors specifically related to OAuth
+    provider login failures, such as misconfiguration, invalid credentials,
+    or unexpected response from the provider.
+    """
+    status_code = status.HTTP_401_UNAUTHORIZED
+    message = "OAuth login error."
+    loc = "oauth"
